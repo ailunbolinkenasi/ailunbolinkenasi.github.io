88dc50255d2f2aa56caaf68cd6be1a814cd34a05
beilanzhisen <beilanzhisen@163.com>
2025-11-25 18:05:24 +0800

    修复移动端搜索组件异常问题

--- src/site.config.ts
+++ src/site.config.ts
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "zh_CN",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "Moment Collector 🌸",
+	title: "Moment Collector",
 };
 
 // Used to generate links in both the Header & Footer.
